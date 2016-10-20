b863be38c7c5dba9ba6624c375a3c1e2b5a042f1
jeremiahwood <jzwood47@cnm.edu>
2016-10-20 09:55:21 -0600

    first draft

---
+++ publc_html/documentation/milestone1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="UTF-32"/>
+		<!--add CSS file-->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<!-- This is the Header-->
+		<header>
+			<h1>Purpose, Audience and Goal!</h1>
+		</header>
+		<!-- this is the main page content-->
+		<main>
+			<!--paragraph of me-->
+			<section>
+				<li>Purpose: This page should convey the different looks of an actor and possabilities for an actor while displaying his experience as an actor and getting him sag acting jobs that lead to A-list parts.</li>
+				<li>Audience: The casting directors, producers and or director that want a fast and professional option of looking at an actors digital resume with there real and contact information.</li>
+				<li>Goal: To reach a larger audience of clients all at once with a wealth of information that leaves a positive and professional impression.</li>
+				<!--begin section 2-->
+			</section>
+			<section>
+				<header>
+					<h2> Persona </h2>
+				</header>
+				<li>As an aspiring actor Ryan Kilgore has two part time jobs and is a student at the community college. He has a  Nikon D5700 and his labtop to record and keep his headshots and film real up to date. He has been trying to break into the entertainment industry for 10 years now and it seems like he is spinning his wheels in the mud. He feels as if others seem to have a bit of an advantage over him.</li>
+			</section>
+			<section>
+
+				<header2>
+					<h3>Use Case</h3>
+				</header2>
+				<ul>
+					<li>The site would be a nicer IMDB of sorts making it an easier and potiental method to cast actors in a production for commercials or films. </li>
+			</section>
+
+
+		</main>
+	</body>
+</html>
