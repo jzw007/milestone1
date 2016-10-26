6dc9228b44443b297f43615b796fcda5c3c4b8a3
jeremiahwood <jzwood47@cnm.edu>
2016-10-25 22:45:02 -0600

    Should be it.

--- public_html/documentation/milestone1.php
+++ public_html/documentation/milestone1.php
@@ -17,13 +17,14 @@
 				<li>Purpose: This page should convey the different looks of an actor and possabilities for an actor while displaying his experience as an actor and getting him sag acting jobs that lead to A-list parts.</li>
 				<li>Audience: The casting directors, producers and or director that want a fast and professional option of looking at an actors digital resume with there real and contact information.</li>
 				<li>Goal: To reach a larger audience of clients all at once with a wealth of information that leaves a positive and professional impression.</li>
-				<!--begin section 2-->
 			</section>
+			<!--begins section 2-->
 			<section>
 				<header>
 					<h2> Persona </h2>
 				</header>
-				<li>As an aspiring actor Ryan Kilgore has two part time jobs and is a student at the community college. He has a  Nikon D5700 and his labtop to record and keep his headshots and film real up to date. He has been trying to break into the entertainment industry for 10 years now and it seems like he is spinning his wheels in the mud. He feels as if others seem to have a bit of an advantage over him.</li>
+				<li> Well Im 35 and rarely act my age and being as I don't have a family or wife I might as well go after the biggest scariest thing for me.As an aspiring actor I am always looking for the professional edge above the rest. I have been trying to break into the entertainment industry for 10 years now with only mediocre results. Since alot of the casting can be done nation wide through technology, why not make it look good? With the ability to send your Video real, Head shots, and resume to agents and audition in New York to L.A. all in a day.
+					</li>
 			</section>
 			<section>
 
@@ -31,7 +32,7 @@
 					<h3>Use Case</h3>
 				</header2>
 				<ul>
-					<li>The site would be a nicer IMDB of sorts making it an easier and potiental method to cast actors in a production for commercials or films. </li>
+					<li>The site would be a nicer IMDB of sorts  but about you, while making it an easier method for casting directors or Producers to locate the actors they need for a production involving commercials, films and telivision. They could easily watch your video real, Headshots and resume. With the option to downlad a digital or printable copy for there own use. </li>
 			</section>
 
 
